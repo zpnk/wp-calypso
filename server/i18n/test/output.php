e7bd309d248b044ef2f80e254b0ae76ee78ad597
Kerry Liu <kerryliu@gmail.com>
2016-05-25 15:11:41 -0700

    Build: update to node 6.2.1 and node-sass to 3.7.0

---
+++ server/i18n/test/output.php
@@ -0,0 +1,70 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. SEE https://github.com/Automattic/wp-calypso/tree/master/server/i18n */
+$arrayName = array(
+__( 'My hat has three corners.' ),
+__( 'My hat has three corners too.' ),
+_n( 'My hat has three corners.', 'My hats have three corners.', 1 ),
+_x( 'post', 'verb' ),
+_x( 'post2', 'verb2' ),
+/* translators: draft saved date format, see http://php.net/date */
+__( 'g:i:s a' ),
+__( 'Your city is %(city)s and your zip is %(zip)s.' ),
+_n( 'single test', 'plural test', 1 ),
+_n( 'single test2', 'plural test2', 1 ),
+__( 'This is a multi-line translation with "mixed quotes" and mixed \'single quotes\'' ),
+_x( 'The string key text', 'context with a literal string key' ),
+__( 'My test has two files.' ),
+
+// Date/time strings for localisation
+_x( "in %s", "future time" ),
+__( "a few seconds" ),
+__( "a minute" ),
+__( "%d minutes" ),
+__( "%d hours" ),
+__( "%d days" ),
+__( "a month" ),
+__( "%d months" ),
+__( "a year" ),
+__( "%d years" ),
+
+// Momentjs formatting strings
+
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "HH:mm", "momentjs format string (for LT)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "DD/MM/YYYY", "momentjs format string (for L)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "D MMMM YYYY", "momentjs format string (for LL)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "D MMMM YYYY LT", "momentjs format string (for LLL)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "dddd, D MMMM YYYY LT", "momentjs format string (for LLLL)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "MMM D", "momentjs format string (for MMM D)" ),
+
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "[Today at] LT", "momentjs format string (sameDay)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "[Tomorrow at] LT", "momentjs format string (nextDay)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "dddd [at] LT", "momentjs format string (nextWeek)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "[Yesterday at] LT", "momentjs format string (lastDay)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "[Last] dddd [at] LT", "momentjs format string (lastWeek)" ),
+/* translators: Moment.js formatting string, see http://momentjs.com/docs/#/displaying/format/ and https://github.com/moment/moment/blob/develop/locale/en-gb.js, surround non-variable text with square brackets */
+_x( "L", "momentjs format string (sameElse)" ),
+
+/* translators: Number representing the first day of the week, 0=Sunday, 1=Monday, etc. Should match typical region formats for the most prominent region where the language is spoken. */
+_x( "num_first_day_of_week", "momentjs setting" ),
+/* translators: The week that contains Jan (X) is the first week of the year, e.g., `4` means Jan 4. Different regions start counting weeks with different rules, check Wikipedia to be sure. */
+_x( "num_day_of_year", "momentjs setting" ),
+
+// Number formatting strings
+/* translators: $thousands_sep argument for http://php.net/number_format, default is , */
+__( "number_format_thousands_sep" ),
+/* translators: $dec_point argument for http://php.net/number_format, default is . */
+__( "number_format_decimal_point" ),
+);
+
+/* THIS IS THE END OF THE GENERATED FILE */
