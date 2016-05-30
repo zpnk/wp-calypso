ca259c46ba82c832bc33a0254c61fd348f8476d1
retrofox <rdsuarez@gmail.com>
2016-05-30 12:03:24 -0300

    wp: add xhr-error-normalizer handler

---
+++ client/lib/wp/handlers/xhr-error-normalizer.js
@@ -0,0 +1,41 @@
+/**
+ * External dependencies
+ */
+import wpcomXhrRequest from 'wpcom-xhr-request';
+import debugFactory from 'debug';
+
+/**
+ * Module variables
+ */
+const debug = debugFactory( 'calypso:wpcom-handler' );
+
+/**
+ * Provides a wrapper around the `wpcom-xhr-request` library
+ * so it returns errors in the same format as `wpcom-proxy-request`.
+ *
+ * @param {Object} params - request parameters
+ * @param {Function} callback - request response callback
+ * @return {Function} handler wrapper
+ */
+const xhrErrorNormalizer = ( params, callback ) => {
+	return wpcomXhrRequest( params, ( error, response ) => {
+		if ( error ) {
+			if ( error.response && typeof error.response.body ) {
+				// Extend the error object in a way to match wpcom-proxy-request
+				error.httpMessage = error.message;
+				error.message = error.response.body.message;
+				error.error = error.response.body.error;
+				error.statusCode = error.status;
+			}
+
+			if ( error.error === 'invalid_token' ) {
+				debug( 'Invalid token error detected, authorisation probably revoked - logging out' );
+				require( 'lib/user/utils' ).logout();
+			}
+		}
+
+		callback( error, response );
+	} );
+};
+
+export default xhrErrorNormalizer;
