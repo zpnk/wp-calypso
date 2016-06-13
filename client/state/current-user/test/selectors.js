96bb093da9659ba445f66a4742600dc45aa88227
Andrew Duthie <andrew@andrewduthie.com>
2016-06-13 09:19:20 -0400

    State: Move canCurrentUser to state/selectors

--- client/state/current-user/test/selectors.js
+++ client/state/current-user/test/selectors.js
@@ -8,8 +8,7 @@ import { expect } from 'chai';
  */
 import {
 	getCurrentUser,
-	getCurrentUserLocale,
-	canCurrentUser
+	getCurrentUserLocale
 } from '../selectors';
 
 describe( 'selectors', () => {
@@ -79,46 +78,6 @@ describe( 'selectors', () => {
 			} );
 
 			expect( locale ).to.equal( 'fr' );
-		} );
-	} );
-
-	describe( 'canCurrentUser', () => {
-		it( 'should return null if the site is not known', () => {
-			const isCapable = canCurrentUser( {
-				currentUser: {
-					capabilities: {}
-				}
-			}, 2916284, 'manage_options' );
-
-			expect( isCapable ).to.be.null;
-		} );
-
-		it( 'should return the value for the specified capability', () => {
-			const isCapable = canCurrentUser( {
-				currentUser: {
-					capabilities: {
-						2916284: {
-							manage_options: false
-						}
-					}
-				}
-			}, 2916284, 'manage_options' );
-
-			expect( isCapable ).to.be.false;
-		} );
-
-		it( 'should return null if the capability is invalid', () => {
-			const isCapable = canCurrentUser( {
-				currentUser: {
-					capabilities: {
-						2916284: {
-							manage_options: false
-						}
-					}
-				}
-			}, 2916284, 'manage_foo' );
-
-			expect( isCapable ).to.be.null;
 		} );
 	} );
 } );
---
+++ client/state/selectors/can-current-user.js
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import get from 'lodash/get';
+
+/**
+ * Returns true if the current user has the specified capability for the site,
+ * false if the user does not have the capability, or null if the capability
+ * cannot be determined (if the site is not currently known, or if specifying
+ * an invalid capability).
+ *
+ * @see https://codex.wordpress.org/Function_Reference/current_user_can
+ *
+ * @param  {Object}   state      Global state tree
+ * @param  {Number}   siteId     Site ID
+ * @param  {String}   capability Capability label
+ * @return {?Boolean}            Whether current user has capability
+ */
+export default function canCurrentUser( state, siteId, capability ) {
+	return get( state.currentUser.capabilities, [ siteId, capability ], null );
+}
