b971a329c175cf5cd5eea814bcac0244fc499bdc
Jordan West <jordwest@gmail.com>
2016-06-14 14:21:28 +1000

    Add blurb

--- client/my-sites/guided-transfer/host-select.jsx
+++ client/my-sites/guided-transfer/host-select.jsx
@@ -12,11 +12,28 @@ import SectionHeader from 'components/section-header';
 
 class HostSelect extends Component {
 	render() {
+		const { translate } = this.props;
+
 		return (
 			<div>
 				<SectionHeader label={ this.props.translate( 'Set up Guided Transfer' ) } />
 				<Card>
-					Content
+					<p>{ translate(
+`{{strong}}Please choose{{/strong}} one of our Guided Transfer compatible
+{{partner_link}}partner hosts{{/partner_link}}. You must have a hosting account
+with one of them to be able to move your site. Visit them {{lobby_link}}Guided
+Transfer Lobby{{/lobby_link}} if you have any question before starting, or
+{{learn_link}}learn more{{/learn_link}} about the process.`,
+						{
+							components: {
+								strong: <strong />,
+								partner_link: <a href="/" />,
+								lobby_link: <a href="/" />,
+								learn_link: <a href="/" />,
+							}
+						} ) }
+					</p>
+					<label>{ translate( 'Select your hosting provider' ) }</label>
 				</Card>
 			</div>
 		);
