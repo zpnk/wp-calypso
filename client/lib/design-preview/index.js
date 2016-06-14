c46e3a815d4519c13cbef86ef2e4c8613ad2accc
Payton Swick <payton@foolord.com>
2016-06-14 18:00:02 -0400

    DesignPreview Lib: Remove deprecated Homepage updater

--- client/lib/design-preview/index.js
+++ client/lib/design-preview/index.js
@@ -1,12 +1,10 @@
 import siteTitle from './updaters/site-title';
 import headerImage from './updaters/header-image';
-import homePage from './updaters/home-page';
 import siteLogo from './updaters/site-logo';
 
 const updaterFunctions = [
 	siteTitle,
 	headerImage,
-	homePage,
 	siteLogo,
 ];
 
