1a479ede50e505a5d9595128a04a8effacb1582c
Umur Kontaci <umur@umur.io>
2016-06-15 16:03:09 -0700

    Domains: Bundle MapDomain

--- client/my-sites/upgrades/map-domain/index.jsx
+++ client/my-sites/upgrades/map-domain/index.jsx
@@ -67,11 +67,28 @@ var MapDomain = React.createClass( {
 	},
 
 	handleRegisterDomain( suggestion ) {
-		upgradesActions.registerDomain( suggestion );
+		const items = cartItems.bundleItemWithPlanIfNecessary(
+			upgradesActions.domainRegistration.fromSuggestion( suggestion ),
+			this.props.domainsWithPlansOnly,
+			this.props.sites.getSelectedSite(),
+			this.props.cart
+		);
+		upgradesActions.addItems( items );
+
+		if ( this.isMounted() ) {
+			page( '/checkout/' + this.props.sites.getSelectedSite().slug );
+		}
 	},
 
 	handleMapDomain( domain ) {
-		upgradesActions.addItem( cartItems.domainMapping( { domain } ) );
+		const items = cartItems.bundleItemWithPlanIfNecessary(
+			cartItems.domainMapping( { domain } ),
+			this.props.domainsWithPlansOnly,
+			this.props.sites.getSelectedSite(),
+			this.props.cart
+		);
+
+		upgradesActions.addItems( items );
 
 		if ( this.isMounted() ) {
 			page( '/checkout/' + this.props.sites.getSelectedSite().slug );
